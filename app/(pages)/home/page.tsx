65142e41968b69a07057beb41a03bde4df5f3b11
s-zehra <zehrataqwi@gmail.com>
2024-12-05 10:35:59 +0300

    feature: add table to the home page

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
 
-const Home = () => {
-  return <div>example page</div>;
-};
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
 
-export default Home;
+export default async function DemoPage() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
